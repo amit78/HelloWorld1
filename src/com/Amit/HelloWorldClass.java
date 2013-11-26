4356f0bbb39114a278395fa5d499900f62553b1c
amit_qlogy <amit.j.joglekar@gmail.com>
2013-11-26 12:23:15 +0530

    Initial commit lkk

--- src/com/Amit/HelloWorldClass.java
+++ src/com/Amit/HelloWorldClass.java
@@ -1,7 +1,7 @@
 package com.Amit;
 
 
-import java.util.logging.XMLFormatter;
+//import java.util.logging.XMLFormatter;
 
 public class HelloWorldClass {
 
@@ -12,9 +12,12 @@ public class HelloWorldClass {
     public static void main(String[] args) {
         System.out.println("Hello World Program  ...!!!!");
         System.out.println("Hey man");
-        Object o=new Object();
+        Object o = Object();
+        System.out.println("Hey man");
+        System.out.println("Hey man");
+        o = new Object();
+        o.getClass().getAnnotations().getClass().desiredAssertionStatus();
 
-        o.
 
     }
 
